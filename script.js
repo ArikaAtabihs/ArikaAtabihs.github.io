3d0033f03e2e81f6b9ea783f87450ce4ecba5332
Akira <92761000+incakolasoda@users.noreply.github.com>
2021-10-22 11:29:11 -1000

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ function logName() {
   //create local storage key with value of a
   localStorage.setItem("uName",a);
   //automatically take user to page two.html
-  location.href = "two.html";
-  
-  //window.alert("Hello " + a);
-  
+  location.href = "two.html";  
   return
 }
 
