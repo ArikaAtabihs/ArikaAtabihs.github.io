e4d631a8d45d2e0aa4af06bf0f746b03b9ae9d48
Akira <92761000+incakolasoda@users.noreply.github.com>
2021-10-22 11:19:17 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function logName() {
-  window.alert(document.getElementById("name").value);
+  //get value from input field and save as a
+  var a = document.getElementById("name").value;
+  //create local storage key with value of a
+  localStorage.setItem("uName",a);
+  //automatically take user to page two.html
+  location.href = "two.html";
+  
+  //window.alert("Hello " + a);
+  
+  return
+}
+
+
+function alertName() {
+  return
 }
